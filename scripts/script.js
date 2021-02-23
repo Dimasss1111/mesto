efaf1131571610627a939463075c9add77ff6dfc
Dimasss1111 <kuznetsovda2000@yandex.ru>
2021-02-23 13:32:16 +0300

    version 1

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 // Открытие попапа
 let popup = document.querySelector('.popup')
-let popupActive = document.querySelector('.person__edit');
+let popupActive = document.querySelector('.person__edit-button');
 
 function popupOpen (){
   popup.classList.add('popup_opened');
@@ -11,31 +11,28 @@ function popupOpen (){
 popupActive.addEventListener('click', popupOpen);
 
 // Закрытие попапа
-let popupDisabled = document.querySelector('.popup__close-icon');
-
-popupDisabled.addEventListener('click',function () {
+function popupClose (){
   popup.classList.remove('popup_opened');
-});
+}
+
+let popupDisabled = document.querySelector('.popup__close-icon-button');
+
+popupDisabled.addEventListener('click', popupClose);
 
 
 // Редактирование профиля
-let personName = document.querySelector('.popup__text_person');// в попапе
+let personName = document.querySelector('.popup__input_person');// в попапе
 let pagePersonName = document.querySelector('.person__name');
-let personMerit = document.querySelector('.popup__text_merits');//в попапе
+let personMerit = document.querySelector('.popup__input_merits');//в попапе
 let pagePersonMerit = document.querySelector('.person__merits');
 
 
-
-personName.addEventListener('focus', function(){personName.value = ''});
-personMerit.addEventListener('focus', function(){personMerit.value = ''});
-
 let saveButton = document.querySelector('.popup__addbutton');
 
-function editProfile() {
+function submitEditProfile (evt) {
+  evt.preventDefault();
   pagePersonName.textContent = personName.value;
   pagePersonMerit.textContent = personMerit.value;
-  personName.value = pagePersonName.textContent;
-  personMerit.value = pagePersonMerit.textContent;
 };
 
-saveButton.addEventListener('click', editProfile);
+saveButton.addEventListener('submit', submitEditProfile);
