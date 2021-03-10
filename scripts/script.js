a74f33a7adee07ea6e1c314a762fe1f7f8206225
Dimasss1111 <kuznetsovda2000@yandex.ru>
2021-03-11 01:53:01 +0300

    added all features

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-//Вставка картинок
 const initialCards = [
   {
     name: 'Архыз',
@@ -26,24 +25,64 @@ const initialCards = [
   }
 ]; 
 
-const container = document.querySelector('.places');
-//const popupEditPersonProfile = document.querySelector('.popup__editPersonProfile');
-const placeForm = document.querySelector('.popup__form_editPlaceList');
-const templateElement = document.querySelector('.template');
 
-//Для вставки исходных карточек
-function createPlaceDomNode(item){
-  const newPlace = templateElement.content.cloneNode(true);
-  const headerPlace = newPlace.querySelector('.place__name');
-  const linkPlace = newPlace.querySelector('.place__photo');
-  headerPlace.textContent = item.name;
-  linkPlace.src = item.link;
-  linkPlace.alt = item.name;
-  return newPlace;
+const container = document.querySelector('.places');
+const placeForm = document.querySelector('.popup__form_addPlaceList');
+const templatePlace = document.querySelector('.template__place');
+
+//Переключение лайка
+function switchLike(evt){
+  const target = evt.target;
+  if (target.getAttribute('src') == './images/like.svg'){
+    target.setAttribute('src','./images/like_active.svg');
+  }
+  else {
+    target.setAttribute('src','./images/like.svg');
+  }
 }
-//Для вставки пользовательских карточек
-function createUserPlaceDomNode(link, name){
-  const newPlace = templateElement.content.cloneNode(true);
+
+//Большая картинка
+function openBigPicture(evt){
+  const target = evt.target;
+  const openedPlace = target.closest('.place');
+  const placeTitle = openedPlace.querySelector('.place__name');
+  const placePhoto = openedPlace.querySelector('.place__photo');
+  const popupPhoto = document.querySelector('.popup__big-picture');
+  const popupTitle = document.querySelector('.popup__title_big-picture');
+  const openPopupPic = popupTitle.closest('.popup');
+  popupTitle.textContent = placeTitle.textContent;
+  popupPhoto.src = placePhoto.src;
+  popupPhoto.alt = placePhoto.alt;
+  openPopupPic.classList.add('popup_opened');
+  openPopupPic.classList.add('popup_opened_big-picture');
+}
+
+
+//Удаление карточки
+function deleteCard(evt){
+  const target = evt.target;
+  const removingCard = target.closest('.place');
+  removingCard.remove();
+}
+
+
+//Слушателu событий карточки
+function addCardListeners(card){
+  const deleteButton = card.querySelector('.place__remove');
+  deleteButton.addEventListener('click', deleteCard);
+
+  const likeButton = card.querySelector('.place__like');
+  likeButton.addEventListener('click', switchLike);
+
+  const bigPicture = card.querySelector('.place__photo');
+  bigPicture.addEventListener('click', openBigPicture);
+}
+
+
+
+//Отрисовка карточек
+function createPlaceDomNode(name, link){
+  const newPlace = templatePlace.content.cloneNode(true);
   const headerPlace = newPlace.querySelector('.place__name');
   const linkPlace = newPlace.querySelector('.place__photo');
   headerPlace.textContent = name;
@@ -52,165 +91,94 @@ function createUserPlaceDomNode(link, name){
   return newPlace;
 }
 
+function createPlaceArguments(item){
+  const nameArg = item.name;
+  const linkArg = item.link;
+  const newPlaceElement = createPlaceDomNode(nameArg, linkArg);
+  addCardListeners(newPlaceElement);
+  return newPlaceElement;
+}
 
 function renderPlaces(){
-  const result = initialCards.map(createPlaceDomNode);
+  const result = initialCards.map(createPlaceArguments);
   container.append(...result);
 }
 
-function addPlaceFormListener(evt) {
+renderPlaces();
+
+
+//Открытие попапов
+const openPopupPerson = document.querySelector('.person__edit-logo');
+const openPopupPlace = document.querySelector('.person__add-place-logo');
+const popupPersonChildren = document.querySelector('.popup__form_editPersonProfile');
+const popupPerson = popupPersonChildren.closest('.popup');
+const popupPlaceChildren = document.querySelector('.popup__form_addPlaceList');
+const popupPlace = popupPlaceChildren.closest('.popup');
+
+openPopupPerson.addEventListener('click', function(){
+  popupPerson.classList.add('popup_opened');
+  personName.value = pagePersonName.textContent;
+  personMerits.value = pagePersonMerits.textContent;
+});
+openPopupPlace.addEventListener('click', ()=> {popupPlace.classList.add('popup_opened')});
+
+//Закрытие попапов
+const popupCloseButton = document.querySelectorAll('.popup__close-icon');
+const popupCloseButtonArr = Array.from(popupCloseButton);
+
+function closePopup(element){
+  element.classList.remove('popup_opened');
+}
+
+function searchElementPopupClose(evt){
+  const closingButton = evt.target;
+  const closingPopup = closingButton.closest('.popup');
+  closePopup(closingPopup);
+}
+
+function searchClickPopupClose(item){
+  item.addEventListener('click', searchElementPopupClose);
+}
+
+popupCloseButtonArr.forEach(searchClickPopupClose);
+
+
+
+//Добавление карточек
+function addCard(evt){
   evt.preventDefault();
   const inputNameForm = placeForm.querySelector('.popup__input_place');
   const inputLinkForm = placeForm.querySelector('.popup__input_link');
   const inputName = inputNameForm.value;
   const inputLink = inputLinkForm.value;
-  console.log(inputLink);
-  const newPlace = createUserPlaceDomNode(inputLink, inputName);
+  const newPlace = createPlaceDomNode(inputName, inputLink);
+  addCardListeners(newPlace);
   container.prepend(newPlace);
-}
-
-renderPlaces();
-
-placeForm.addEventListener('submit', addPlaceFormListener);
-
-
-
-
-//Попытка(успешная) сделать универсальный слушатель открытия попапов
-
-//Обработка кнопок открытия
-const openButtonAll = document.querySelectorAll('.person__button');
-const openButton = Array.from(openButtonAll);
-
-function openingPopup(keyword){
-  const thisPopup = keyword.closest('.popup');//получаем ближайшего родителя с классом popup
-  thisPopup.classList.add('popup_opened');
-}
-
-function comparing(text, mainText){
-  const mainTextPrepared = mainText.classList.value;//Получаем строку списка классов
-  const searchText = text.toLowerCase();
-  const searchMainText = mainTextPrepared.toLowerCase();//сравниваем искомый текст со строкой списка классов
-  if(searchMainText.includes(searchText)){
-    openingPopup(mainText);
-  }
-
-}
-
-function searchPopup(inputVar){
-  const forms = document.querySelectorAll('.popup__form');
-  const formsArr = Array.from(forms);
-  const requredText = inputVar.slice(inputVar.length - 9, inputVar.length - 5);//отрезаем искомый текст
-  for (let i = 0; i < formsArr.length; i++) { //для полученных элементов сравниваем искомый текст
-    comparing(requredText, (formsArr[i]));
-  }
   
+  searchElementPopupClose(evt);//Использую ранее описанную функцию для закрытия формы по кнопке 'Сохранить'
+  inputNameForm.value = '';
+  inputLinkForm.value = '';
 }
 
-function searchClickedElement(evt){
-    const clickedButton = evt.target;
-    const clickedButtonClassList = Array.from(clickedButton.classList);
-    const exportVar = clickedButtonClassList[clickedButtonClassList.length - 1];//находим последний класс из списка
-    searchPopup(exportVar);//отправляем строку с названием класса
-};
 
-function searchClick(item){
-  item.addEventListener('click', searchClickedElement);
-}
-openButton.forEach(searchClick);//Отслеживание клика на каждой кнопке открытия попапа
+placeForm.addEventListener('submit', addCard);
 
 
+//Редактирование профиля
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-// Открытие попапа УЧЕНОГО
-let popup = document.querySelector('.popup')
-let popupPersonActive = document.querySelector('.person__button_edit');
-
-function popupOpen (){
-  popup.classList.add('popup_opened');
-  personName.value = pagePersonName.textContent;
-  personMerit.value = pagePersonMerit.textContent;
-};
-
-popupPersonActive.addEventListener('click', popupOpen);
-
-
-// Закрытие попапа УЧЕНОГО
-function popupClose (){
-  popup.classList.remove('popup_opened');
-}
-
-let popupPersonDisabled = document.querySelector('.popup__close-icon-button');
-
-popupPersonDisabled.addEventListener('click', popupClose);
-
-// Редактирование профиля УЧЕНОГО
-let personName = document.querySelector('.popup__input_person');// в попапе
-let pagePersonName = document.querySelector('.person__name');
-let personMerit = document.querySelector('.popup__input_merits');//в попапе
-let pagePersonMerit = document.querySelector('.person__merits');
-
-
-let saveButton = document.querySelector('.popup__addbutton');
+//Изменение данных профиля
+const popupEditPersonProfile = document.querySelector('.popup__form_editPersonProfile');
+const pagePersonName = document.querySelector('.person__name');
+const pagePersonMerits = document.querySelector('.person__merits');
+const personName = document.querySelector('.popup__field_input_person');
+const personMerits = document.querySelector('.popup__field_input_merits');
 
 function submitEditProfile (evt) {
   evt.preventDefault();
   pagePersonName.textContent = personName.value;
-  pagePersonMerit.textContent = personMerit.value;
+  pagePersonMerits.textContent = personMerits.value;
+  console.log(pagePersonName.textContent);
+  searchElementPopupClose(evt);//Использую ранее описанную функцию для закрытия формы по кнопке 'Сохранить'
 };
 
-saveButton.addEventListener('submit', submitEditProfile);
-
-
-
-//Открытие попапа ЛОКАЦИИ
-let place = document.querySelector('.place');
-let openButton = document.querySelectorAll('.person__button');
-
-openButton.addEventListener('click',function(event){
-  console.log(event.target);
-})
-*/
+popupEditPersonProfile.addEventListener('submit', submitEditProfile);
